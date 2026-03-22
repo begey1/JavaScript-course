9662d5b579be6ad027b66ed615664454b09a15f6
begey1 <146122765+begey1@users.noreply.github.com>
2026-03-23 00:27:35 +0200

    Add last letter check for names in script.js

--- lab3/script.js
+++ lab3/script.js
@@ -15,3 +15,24 @@
     }
 
 })();
+
+(function () {
+
+    console.log("=== Additional feature: last letter check ===");
+
+    var names = ["Bill", "John", "Jen", "Jason", "Paul", "Frank", "Steven", "Larry", "Paula", "Laura", "Jim"];
+
+    for (var i = 0; i < names.length; i++) {
+
+        // беремо останню літеру імені
+        var lastLetter = names[i].charAt(names[i].length - 1).toLowerCase();
+
+        if ("aeiou".includes(lastLetter)) {
+            console.log(names[i] + " ends with a vowel");
+        } else {
+            console.log(names[i] + " ends with a consonant");
+        }
+
+    }
+
+})();
