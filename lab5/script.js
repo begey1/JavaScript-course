685dff75760a851b7af9b5dee4fa48f59b4c63de
begey1 <146122765+begey1@users.noreply.github.com>
2026-04-19 21:47:57 +0300

    Update script.js

--- lab5/script.js
+++ lab5/script.js
@@ -1,1 +1,53 @@
+let score = 0;
+let time = 0;
+let timer;
+let correctColor;
 
+function startGame(level) {
+    document.getElementById("menu").classList.add("hidden");
+    document.getElementById("game").classList.remove("hidden");
+
+    score = 0;
+
+    if (level === "easy") time = 20;
+    if (level === "medium") time = 15;
+    if (level === "hard") time = 10;
+
+    document.getElementById("time").textContent = time;
+    document.getElementById("score").textContent = score;
+
+    nextRound();
+
+    timer = setInterval(() => {
+        time--;
+        document.getElementById("time").textContent = time;
+
+        if (time <= 0) {
+            clearInterval(timer);
+            alert("Гра закінчена! Очки: " + score);
+        }
+    }, 1000);
+}
+
+function nextRound() {
+    let colors = ["red", "green", "blue"];
+    correctColor = colors[Math.floor(Math.random() * 3)];
+
+    document.querySelectorAll(".square").forEach(sq => {
+        sq.style.opacity = "0.3";
+    });
+
+    document.getElementById(correctColor).style.opacity = "1";
+}
+
+function clickSquare(color) {
+    if (color === correctColor) {
+        score++;
+        document.getElementById("score").textContent = score;
+        nextRound();
+    }
+}
+
+function restart() {
+    location.reload();
+}
