d1195873f20a90351c0a89e823230f3be3d8af97
begey1 <146122765+begey1@users.noreply.github.com>
2026-05-03 16:26:38 +0300

    Update script.js

--- lab8/script.js
+++ lab8/script.js
@@ -1,1 +1,43 @@
+const burger = document.getElementById("burger");
+const menu = document.getElementById("menu");
 
+burger.onclick = () => {
+    menu.classList.toggle("active");
+};
+
+let index = 0;
+const slides = document.getElementById("slides");
+const total = slides.children.length;
+
+// dots
+const dotsContainer = document.getElementById("dots");
+
+for (let i = 0; i < total; i++) {
+    let dot = document.createElement("span");
+    dot.onclick = () => moveTo(i);
+    dotsContainer.appendChild(dot);
+}
+
+function updateSlider() {
+    slides.style.transform = `translateX(-${index * 100}%)`;
+}
+
+function moveTo(i) {
+    index = i;
+    updateSlider();
+}
+
+document.querySelector(".next").onclick = () => {
+    index = (index + 1) % total;
+    updateSlider();
+};
+
+document.querySelector(".prev").onclick = () => {
+    index = (index - 1 + total) % total;
+    updateSlider();
+};
+
+setInterval(() => {
+    index = (index + 1) % total;
+    updateSlider();
+}, 3000);
